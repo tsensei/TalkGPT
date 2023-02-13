cfa7158a28a155deafff2b9fe3f4a5454bc69c2f
tsensei <tjsiam019@gmail.com>
2023-02-13 13:59:49 +0600

    First Deploy

---
+++ index.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(express.static(path.join(__dirname, "build")));
+
+app.post("/api/generate", async (req, res) => {
+  const prompt = req.body.prompt;
+  console.log("Hit");
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 300,
+    });
+    console.log("Got");
+    res.status(200).json({
+      status: "success",
+      message: completion.data.choices[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: "failed",
+      message: "Some error occured",
+    });
+  }
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
